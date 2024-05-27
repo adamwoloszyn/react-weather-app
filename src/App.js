ba2a8e9af3a12bcb69d68d2efd83a53a5692fd05
Adam Woloszyn <adamjwoloszyn@gmail.com>
2024-05-27 12:19:13 -0600

    testing out react push

--- src/App.js
+++ src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState('');
+  const [weather, setWeather] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault(); 
+    try {
+      const base_url = 'https://python-weather-dot-adam-workshop.uw.r.appspot.com';
+      const response = await fetch(`${base_url}/get_weather_response?city=${encodeURIComponent(city)}`);
+      const data = await response.json(); 
+
+      if (data.cod !== 200) {
+        throw new Error(data.message);
+      }else {
+        setWeather(data);
+      }
+
+    } catch (error) {
+      console.error('Failed to fetch weather data:', error);
+      setWeather({ error: 'Failed to fetch weather data' });
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Get Weather Conditions</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={city}
+            onChange={e => setCity(e.target.value)}
+            placeholder="Enter City"
+            aria-label="City"
+          />
+          <button type="submit">Get Weather</button>
+        </form>
+        {weather && (
+          <div>
+            {weather.error ? (
+              <p>Error: {weather.error}</p>
+            ) : (
+              <div>
+                <h2>Weather in {weather.name}</h2>
+                <p>{weather.weather[0].description}</p>
+                <p>Temperature: {weather.main.temp}°C</p>
+                <p>Feels like: {weather.main.feels_like}°C</p>
+              </div>
+            )}
+          </div>
+        )}
       </header>
     </div>
   );
